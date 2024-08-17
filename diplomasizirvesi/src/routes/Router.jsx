dc9d928fa002362354f012500015f539f69e185e
mmehtapavci <mmmehtapavci@gmail.com>
2024-08-17 21:29:47 +0300

    DiplomasiZirvesiedit
    Co-Authored-By: Furkan06-k <109735879+Furkan06-k@users.noreply.github.com>
    Co-Authored-By: camci470 <127327351+camci470@users.noreply.github.com>
    Co-Authored-By: Ahmet KANBAZ <106634419+ahmetkanbaz@users.noreply.github.com>

--- diplomasizirvesi/src/routes/Router.jsx
+++ diplomasizirvesi/src/routes/Router.jsx
@@ -2,8 +2,20 @@ import { useRoutes } from "react-router-dom";
 import Home from "../views/Home/Home";
 
 const Router = () => {
+<<<<<<< Updated upstream
   const routes = useRoutes([{ path: "/", element: <Home /> }]);
   return routes;
 };
 
 export default Router;
+=======
+    const routes = useRoutes(
+        [
+            { path: '/', element: < Home />}
+        ]
+    )
+    return routes
+}
+
+export default Router
+>>>>>>> Stashed changes
