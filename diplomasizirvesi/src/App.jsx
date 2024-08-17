a6aae8aa75d30fefaef0199cca47ebc782621e68
Ahmet KANBAZ <ahmetkanbaz@gmail.com>
2024-08-17 21:24:40 +0300

    DiplomasiZirvesi
    Router, Navbar ve Footer temelleri oluşturuldu.

--- diplomasizirvesi/src/App.jsx
+++ diplomasizirvesi/src/App.jsx
@@ -1,8 +1,14 @@
+import Navbar from './components/Navbar/Navbar'
+import Router from './routes/Router'
+import Footer from './components/Footer/Footer'
+
 function App() {
 
   return (
     <>
-      Hello World!
+      <Navbar />
+      <Router />
+      <Footer />
     </>
   )
 }
